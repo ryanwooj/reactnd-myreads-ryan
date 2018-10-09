87d1ec363fc1b0110fbef65c23a1ad09d1226e7b
Ryan Woo <woo@Ryans-MacBook-Pro.local>
2018-10-09 16:54:20 -0700

    Add ListBooks, fix errors, add shelves.map() to make code cleaner

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React { Component } from 'react'
+import React, { Component } from 'react'
 import { Route } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI'
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
 import './App.css'
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
+
   state = {
-    books : [],
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books : []
   }
 
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({books})
+      // console.log(books);
     })
+  }
+
+  optionChange = (book, shelf) => {
+      BooksAPI.update(book,shelf).then(() => {
+        BooksAPI.getAll().then((books) => {
+          this.setState({ books })
+        })
+      })
   }
 
   render() {
@@ -29,33 +32,20 @@ class BooksApp extends React.Component {
         <Route
           exact path="/"
           render={() => (
-
+            <ListBooks
+              optionChange={this.optionChange}
+              booksOnShelf={this.state.books}
+            />
           )}
         />
 
         <Route
           path="/search"
-          render={() => (
-            <div className="search-books">
-              <div className="search-books-bar">
-                <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-                <div className="search-books-input-wrapper">
-                  {/*
-                      NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                      You can find these search terms here:
-                      https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                      However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                      you don't find a specific author or title. Every search is limited by search terms.
-                  */}
-                  <input type="text" placeholder="Search by title or author"/>
-
-                </div>
-              </div>
-              <div className="search-books-results">
-                <ol className="books-grid"></ol>
-              </div>
-            </div>
+          render={({history}) => (
+            <SearchBooks
+              optionChange={this.optionChange}
+              booksOnShelf={this.state.books}
+            />
           )}
         />
 
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,1 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Books from './Books.js'
+
+class ListBooks extends React.Component {
+
+  render() {
+    const { booksOnShelf, optionChange } = this.props
+    const shelves = ["currentlyReading", "wantToRead", "read"]
+    const status = ["Curently Reading", "Want To Read", "Read"]
+
+    return (
+
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>Ryan's Reads</h1>
+        </div>
+        <div className="list-books-content">
+          {shelves.map((shelve, index) => (
+            <div className="bookshelf" key={index}>
+              <h2 className="bookshelf-title">{status[index]}</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {booksOnShelf.filter(book => book.shelf === shelves[index])
+                    .map(book => (
+                      <li key={book.id}>
+                        <Books
+                          book = {book}
+                          optionChange={optionChange}
+                          currentShelf= {shelve}
+                        />
+                      </li>)
+                    )
+                  }
+                </ol>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="open-search">
+          <Link
+          to ='/search'>Add a book</Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+ListBooks.propTypes = {
+  booksOnShelf: PropTypes.array.isRequired,
+  optionChange: PropTypes.func.isRequired
+};
+
+export default ListBooks
