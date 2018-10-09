20b8328ca6816db99f031f0d343002ac04832d7d
Ryan Woo <woo@Ryans-MacBook-Pro.local>
2018-10-09 16:02:20 -0700

    Add Books.js for later use

---
+++ src/Books.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Books extends Component {
+  render() {
+    const { book } = this.props;
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 120,
+              height: 180,
+              backgroundImage:`url("${book.imageLinks.smallThumbnail}")`
+            }}>
+          </div>
+          <div className="book-shelf-changer">
+            <select
+              onChange= {(event) => this.props.optionChange(
+              this.props.book, event.target.value)}
+              value={this.props.currentShelf}
+            >
+              <option value="move" disabled> Move to...</option>
+              <option value="wantToRead">Want To Read</option>
+              <option value="currentlyReading">Curently Reading</option>
+              <option value="read">Read</option>
+              <option value="none">none</option>
+            </select>
+          </div>
+        </div>
+        <p className="book-title">{this.props.book.title}</p>
+        <p className="book-authors">{this.props.book.authors}</p>
+      </div>
+    );
+  }
+}
+
+export default Books
